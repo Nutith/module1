d701df3c8c298bd135d80c5efb3c97ba343b6d79
Nutith <dvedenev@hotmail.com>
2024-09-01 15:49:40 +0300

    решение

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите строку: ')
+print('Количество введенных символов:', len(my_string))
+
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка с удаленными пробелами:', my_string.replace(' ', ''))
+print('Первый символ строки:', my_string[0])
+print('Последний символ строки:', my_string[-1])
